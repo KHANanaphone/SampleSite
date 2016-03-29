fc5c56ae9cdb985611b08e5c844d9501747206f7
Adan Ferguson <adanferguson@gmail.com>
2016-03-29 05:11:28 -0600

    Products page

--- www/js/app/products_controller.js
+++ www/js/app/products_controller.js
@@ -2,16 +2,32 @@
 
     var app = angular.module('sample-app');
 
-    app.controller('products-controller', ['$scope', function($scope){
+    app.controller('products-controller', ['$scope', '$timeout', function($scope, $timeout){
 
         $scope.products = {
 
-            'Product A': {},
-            'Product B': {},
-            'Product C': {},
-            'Product D': {}
-
+            'A': {id: 'A', image: 'stock1.jpg', side: 'left', color: 'blue'},
+            'B': {id: 'B', image: 'stock2.jpg', side: 'right', color: 'orange'},
+            'C': {id: 'C', image: 'stock3.jpg', side: 'left', color: 'purple'},
+            'D': {id: 'D', image: 'stock4.jpg', side: 'right', color: 'green'}
         };
+
+        $scope.select = function(i){
+
+            if($scope.selected.id == i)
+                return;
+
+            $scope.fadeout = true;
+
+            $timeout(function(){
+
+                $scope.selected = $scope.products[i];
+                $timeout(() => $scope.fadeout = false, 200);
+
+            }, 200)
+        }
+
+        $scope.selected = $scope.products['A']
 
     }]);
 
